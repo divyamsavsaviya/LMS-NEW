74d0850e5e9c3f08a3745da0014991707b64429a
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2024-05-10 17:05:16 -0700

    feat: updated backend to novel backend

--- backend/models/studentSchema.js
+++ backend/models/studentSchema.js
@@ -1,60 +1,74 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  rollNum: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  sclassName: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "sclass",
+    required: true,
+  },
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "admin",
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "Student",
+  },
+  subjects: [
+    {
+      subName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "subject",
+      },
     },
-    rollNum: {
+  ],
+  examResult: [
+    {
+      subName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "subject",
+      },
+      marksObtained: {
         type: Number,
-        required: true
-    },
-    password: {
+        default: 0,
+      },
+      description: {
         type: String,
-        required: true
+        default: "Assignment",
+      },
     },
-    sclassName: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'sclass',
+  ],
+  attendance: [
+    {
+      date: {
+        type: Date,
         required: true,
-    },
-    school: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'admin',
-        required: true,
-    },
-    role: {
+      },
+      status: {
         type: String,
-        default: "Student"
+        enum: ["Present", "Absent"],
+        required: true,
+      },
+      subName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "subject",
+        required: true,
+      },
     },
-    examResult: [
-        {
-            subName: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'subject',
-            },
-            marksObtained: {
-                type: Number,
-                default: 0
-            }
-        }
-    ],
-    attendance: [{
-        date: {
-            type: Date,
-            required: true
-        },
-        status: {
-            type: String,
-            enum: ['Present', 'Absent'],
-            required: true
-        },
-        subName: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'subject',
-            required: true
-        }
-    }]
+  ],
 });
 
 module.exports = mongoose.model("student", studentSchema);
--- backend/models/subjectSchema.js
+++ backend/models/subjectSchema.js
@@ -1,40 +1,71 @@
 const mongoose = require("mongoose");
 
-const subjectSchema = new mongoose.Schema({
+const subjectSchema = new mongoose.Schema(
+  {
+    subjectStatus: {
+      type: String,
+      required: true,
+      default: "published",
+    },
     subName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     subCode: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     sessions: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-   
-   
-   ///Adding the course description
-    //courseDescription: { 
-      //type: String,
-     ///   required: true,
-  //  },
-
-
-
     sclassName: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'sclass',
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "sclass",
+      required: true,
     },
     school: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'admin'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "admin",
     },
     teacher: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'teacher',
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "teacher",
     },
-});
+    syllabus: {
+      type: String,
+      default: "",
+    },
+    announcements: [
+      {
+        type: {
+          type: String,
+          default: "",
+        },
+        title: {
+          type: String,
+          default: "",
+        },
+        description: {
+          type: String,
+          default: "",
+        },
+        marks: {
+          type: Number,
+          default: 0,
+        },
+        deadline: {
+          type: String,
+          default: "",
+        },
+        postedOn: {
+          type: String,
+          default: "",
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
 module.exports = mongoose.model("subject", subjectSchema);
